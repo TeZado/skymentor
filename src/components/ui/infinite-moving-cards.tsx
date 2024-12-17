a63ee608b1dee75e204c53995bd15d977d80f900
Hrishikesh <hrishikesh082000@gmail.com>
2024-12-17 11:26:49 +0530

    testimonials updated

--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -104,7 +104,7 @@ export const InfiniteMovingCards = ({
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
-                  <span className=" text-sm leading-[1.6] text-black font-bold">
+                  <span className=" absolute top-24 text-sm leading-[1.6] text-black font-bold">
                     {item.name}
                   </span>
                   <span className=" text-sm leading-[1.6] text-black font-normal">
