83fd682eb23e16e7079fad89078529d3ace88241
Hrishikesh <hrishikesh082000@gmail.com>
2024-12-18 12:27:35 +0530

    updated

--- src/app/Blog/[blogpost]/page.tsx
+++ src/app/Blog/[blogpost]/page.tsx
@@ -1,41 +1,54 @@
 import BlogData from "@/data/blog.json";
 import Image from "next/image";
 
-export default function page({ params }: any) {
+export default function Page({ params }: { params: { blogpost: string } }) {
+  const blogIndex = parseInt(params.blogpost) - 1; // Convert to number and zero-based index
+  const blog = BlogData[blogIndex];
+
+  if (!blog) {
+    // Handle invalid or out-of-range blogpost param
+    return (
+      <div className="w-[90%] lg:w-[70%] m-auto text-center mt-8">
+        <h1 className="font-bold text-2xl md:text-4xl lg:text-5xl">
+          Blog Post Not Found
+        </h1>
+        <p className="text-lg mt-4">The blog post you are looking for does not exist.</p>
+      </div>
+    );
+  }
+
   return (
     <>
+      <head>
+        <title>{blog.title} | Blog</title>
+        <meta name="description" content={blog.subtitle} />
+      </head>
       <div className="w-[90%] lg:w-[70%] m-auto">
-        <div className="mt-8">
-          <h1 className="font-bold text-2xl md:text-4xl lg:text-5xl text-center ">
-            {BlogData[params.blogpost - 1].title}
+        <div className="mt-8 text-center">
+          <h1 className="font-bold text-2xl md:text-4xl lg:text-5xl">
+            {blog.title}
           </h1>
-          <h1 className=" md:text-lg lg:text-xl text-center my-2 lg:my-4">
-            {BlogData[params.blogpost - 1].subtitle}
-          </h1>
+          <h2 className="md:text-lg lg:text-xl my-2 lg:my-4">
+            {blog.subtitle}
+          </h2>
         </div>
 
         <Image
-          className="m-auto my-10 h-[350px] object-contain"
+          className="m-auto my-10 h-auto max-h-[350px] object-contain"
           width={900}
           height={900}
-          src={BlogData[params.blogpost - 1].image}
-          alt="image"
+          src={blog.image}
+          alt={blog.title}
         />
 
-        {BlogData[params.blogpost - 1].data?.map(
-          ({ title, description }, index) => {
-            return (
-              <>
-                <div key={index + title}>
-                  <h1 className="font-bold lg:text-xl my-2">
-                    {index + 1} ) {title}
-                  </h1>
-                  <p className="px-2 mb-4 lg:text-xl">{description}</p>
-                </div>
-              </>
-            );
-          }
-        )}
+        {blog.data?.map(({ title, description }, index) => (
+          <div key={`${index}-${title}`}>
+            <h3 className="font-bold lg:text-xl my-2">
+              {index + 1}. {title}
+            </h3>
+            <p className="px-2 mb-4 lg:text-xl">{description}</p>
+          </div>
+        ))}
       </div>
     </>
   );
--- src/components/ui/staffcards.tsx
+++ src/components/ui/staffcards.tsx
@@ -2,13 +2,23 @@ import Image from "next/image";
 import React from "react";
 import Linkdin from "@/app/assets/images/linkdin.png";
 
-export default function staffcards({ image, link, name, post }: any) {
+export default function staffcards({
+  image,
+  link,
+  name,
+  post,
+}: {
+  image: string;
+  link: string;
+  name: string;
+  post: string;
+}) {
   return (
     <>
       <div className="hidden lg:block border-2 w-[200px] h-[360px] md:w-[300px] md:h-[430px] p-6 border-[#ffefad] rounded-2xl">
         <div>
           <div>
-            <Image className="w-[300px] rounded-2xl" src={image} alt="#" />
+            <Image className="w-[300px] rounded-xl" src={image} alt="#" />
           </div>
           <div className="flex flex-row justify-between">
             <div className="mt-2 ml-2">
@@ -28,7 +38,7 @@ export default function staffcards({ image, link, name, post }: any) {
       <div className="lg:hidden border-2 w-[180px] h-[350px] lg:w-[300px] lg:h-[430px] p-2 border-[#ffefad] rounded-2xl">
         <div>
           <div>
-            <Image className="w-[300px] rounded-2xl" src={image} alt="#" />
+            <Image className="w-[300px] rounded-xl" src={image} alt="#" />
           </div>
           <div className="flex flex-row justify-between">
             <div className="mt-2 ml-2">
@@ -36,11 +46,11 @@ export default function staffcards({ image, link, name, post }: any) {
               <p className="h-[50px]">{post}</p>
             </div>
           </div>
-            <div className="relative bottom-0 ml-1">
-              <a href={link} target="blank">
-                <Image width={40} src={Linkdin} alt="#" />
-              </a>
-            </div>
+          <div className="relative bottom-0 ml-1">
+            <a href={link} target="blank">
+              <Image width={40} src={Linkdin} alt="#" />
+            </a>
+          </div>
         </div>
       </div>
     </>
