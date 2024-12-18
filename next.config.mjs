71b5ce65b1567787dc43b6aba2a8b713a3f40ba8
Hrishikesh <hrishikesh082000@gmail.com>
2024-12-18 12:38:20 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import { hostname } from 'os';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint during the build process
+  },
     images: {
         domains: ['images.unsplash.com',"cdn.prod.website-files.com","img.freepik.com"],
       },
